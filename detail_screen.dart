8e06b88cb4c65b601e5c89a12090493300547882
Saniputri1112 <pratiwisaniputri@gmail.com>
2024-11-23 18:08:37 +0900

    Add files via upload

---
+++ detail_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String title;
+  final String description;
+  final String imageUrl;
+
+  DetailScreen({
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Detail News')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(imageUrl),
+            const SizedBox(height: 16),
+            Text(title, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 16),
+            Text(description, style: const TextStyle(fontSize: 16)),
+          ],
+        ),
+      ),
+    );
+  }
+}
